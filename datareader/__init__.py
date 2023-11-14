15131bcc4740ec080fd58a1a475a068d47b9b83f
Ted <teddychnster@gmail.com>
2023-11-14 22:07:45 +0800

    easy data reading

--- datareader/__init__.py
+++ datareader/__init__.py
@@ -1,2 +1,22 @@
 from .read_nibrs_raw import read_NIBRS_file_paths
 from .BHReader import *
+import pandas as pd
+from pathlib import Path
+
+
+def all_df_reader(pk_path: Path|str) -> pd.DataFrame:
+
+    all_df = pd.read_pickle(pk_path)
+    all_df = all_df[pd.to_numeric(all_df['NUMBER OF MONTHS REPORTED'], errors='coerce').notnull()]
+    all_df["NUMBER OF MONTHS REPORTED"] = pd.to_numeric(all_df["NUMBER OF MONTHS REPORTED"])
+    return all_df
+
+
+def _to_selected_agency_info(df:pd.DataFrame) -> pd.DataFrame:
+    coords = df[["ori", "agency_name_full","longitude", "latitude"]].rename(columns={"ori":"ORI"})
+    return coords
+
+def geo_reader(csv_path:Path|str) -> pd.DataFrame:
+    bs_df = pd.read_csv(csv_path)    # baseline
+    geo_info = _to_selected_agency_info(bs_df)
+    return geo_info
