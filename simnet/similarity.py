bae43e01a4bcda18dc0bcc84c8e85fde7634c3ac
Ted <teddychnster@gmail.com>
2023-11-22 14:27:17 +0800

    Format inputed data and create sim network

--- simnet/similarity.py
+++ simnet/similarity.py
@@ -11,7 +11,7 @@ class Similarity(ABC):
     Base class for similarity (or dissimilarity) metric
     """
     @abstractmethod
-    def find_similarity_matrix(self, df:np.ndarray):
+    def find_similarity_matrix(self, df:np.ndarray, weight):
         """
         Returns the booleans similarity matrix, given a criteria or threshold
         provided in the construction stage.
@@ -26,9 +26,16 @@ class CosineSimilarity(Similarity):
     def __init__(self, threshold:float):
         self._th = threshold
 
-    def find_similarity_matrix(self, df):
-       self.similarity_matrix = cosine_similarity(df)
-       return self.similarity_matrix > self._th
+    def find_similarity_matrix(self, df:np.ndarray, weight:np.ndarray|None = None):
+        if weight is None:
+            weight = np.ones(df.shape[1]) / df.shape[1]
+
+        w_sqrt = np.sqrt(weight)
+        if df.shape[1] != w_sqrt.shape[0]:
+            raise ShapeMismatchError(f"Shape for each row in df is {df.shape[1]}, different from shape of weight vector {w_sqrt.shape[0]}")
+
+        self.similarity_matrix = cosine_similarity(np.multiply(df, w_sqrt))
+        return self.similarity_matrix > self._th
 
 class MatchNumberSimilarity(Similarity):
     """
@@ -38,7 +45,7 @@ class MatchNumberSimilarity(Similarity):
     def __init__(self, threshold:int):
         self._th = threshold
 
-    def find_similarity_matrix(self, df):
+    def find_similarity_matrix(self, df, weight):
         _df_sparse = csr_matrix(df)
         self.similarity_matrix = _df_sparse@_df_sparse.T
         return self.similarity_matrix >= self._th
@@ -53,15 +60,16 @@ class SimilarityNetwork:
     def __init__(self, df:pd.DataFrame, similarity_measure: Similarity, index_column:str, match_columns:list[str]):
         self._df: pd.DataFrame = df.set_index(index_column)[match_columns] # type: ignore
         self._df = pd.get_dummies(self._df)
+        self.feature_n = len(self._df.columns)
         self.sm = similarity_measure
         self.nodes = self._df.index.to_list()
 
-    def fit_transform(self):
+    def fit_transform(self, weight = None):
         """
         Computes the adjacency matrix. Return the matrix in a sparse matrix format.
         """
         self.adjacency_matrix = csr_matrix(
-            self.sm.find_similarity_matrix(self._df.to_numpy())
+            self.sm.find_similarity_matrix(self._df.to_numpy(), weight)
         )
         return self.adjacency_matrix
 
@@ -73,3 +81,6 @@ class SimilarityNetwork:
         """
         _G  = nx.from_numpy_array(self.adjacency_matrix)
         return nx.relabel_nodes(_G, {i:n for i, n in enumerate(self.nodes)})
+
+class ShapeMismatchError(ValueError):
+    pass
